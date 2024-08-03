31dd6ed64a3d9b9e52ad439b1cb9c9bf28873a39
Hugo Bouderlique <bouderliqueh@gmail.com>
2024-08-03 22:02:55 +1000

    rework cartesian axis

--- src/cartesian_axis.rs
+++ src/cartesian_axis.rs
@@ -1,7 +1,42 @@
-use web_sys::WebGlProgram;
+use web_sys::{WebGl2RenderingContext, WebGlProgram};
 
 use crate::colour::Colour;
 use crate::matrix::Matrix4F;
 use crate::primitives::{Draw, Line, Vertex};
 
-pub struct CartesianAxis;
+pub struct CartesianAxis<'a> {
+    lines: [Line<'a>; 3],
+}
+
+impl<'a> CartesianAxis<'a> {
+    pub fn new(
+        context: &WebGl2RenderingContext,
+        program: &'a WebGlProgram,
+    ) -> CartesianAxis<'a> {
+        let o = Vertex::new(0.0, 0.0, 0.0);
+        let i = Vertex::new(1.0, 0.0, 0.0);
+        let j = Vertex::new(0.0, 1.0, 0.0);
+        let k = Vertex::new(0.0, 0.0, 1.0);
+
+        let x_axis = Line::new(context, o, i, Colour::RED, program);
+        let y_axis = Line::new(context, o, j, Colour::GREEN, program);
+        let z_axis = Line::new(context, o, k, Colour::BLUE, program);
+
+        return CartesianAxis {
+            lines: [x_axis, y_axis, z_axis],
+        };
+    }
+}
+
+impl<'a> Draw for CartesianAxis<'a> {
+    fn draw(
+        &self,
+        context: &WebGl2RenderingContext,
+        model_matrix: Option<Matrix4F>,
+    ) -> Result<(), String> {
+        for line in &self.lines {
+            line.draw(context, model_matrix)?;
+        }
+        return Ok(());
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -87,28 +87,30 @@ fn run(
     // let cart = CartesianAxis::new(program);
     // cart.draw(context, None)?;
     //
-    let t = Triangle::new(
-        context,
-        Vertex::new(-0.5, 0.0, -0.3),
-        Vertex::new(0.0, 0.5, -0.6),
-        Vertex::new(0.5, 0.0, -0.6),
-        Colour::PINK,
-        program,
-    );
-
-
-    let b = boxx::Box::new(
-        context,
-        0.5,
-        -0.5,
-        -0.5,
-        -0.5,
-        0.5,
-        0.5,
-        Colour::RED,
-        program,
-    );
-    b.draw(&context, None)?;
-    t.draw(&context, None)?;
+    // let t = Triangle::new(
+    //     context,
+    //     Vertex::new(-0.5, 0.0, -0.3),
+    //     Vertex::new(0.0, 0.5, -0.6),
+    //     Vertex::new(0.5, 0.0, -0.6),
+    //     Colour::PINK,
+    //     program,
+    // );
+    //
+    //
+    // let b = boxx::Box::new(
+    //     context,
+    //     0.5,
+    //     -0.5,
+    //     -0.5,
+    //     -0.5,
+    //     0.5,
+    //     0.5,
+    //     Colour::RED,
+    //     program,
+    // );
+    // b.draw(&context, None)?;
+    // t.draw(&context, None)?;
+    let ca = CartesianAxis::new(context, program);
+    ca.draw(context, None)?;
     return Ok(());
 }
